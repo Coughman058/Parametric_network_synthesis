044fd813e7d5efdb3d923060c44e0c47a19d75cf
Ryan Kaufman <51339132+Coughman058@users.noreply.github.com>
2023-12-05 16:40:13 -0500

    Fixing module structure and setup

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='Parametric_Network_Synthesis',
     version='0.0.1',
-    packages = ['parametricSynthesis'],
+    packages = ['parametricSynthesis',
+                'parametricSynthesis.network_tools',
+                'parametricSynthesis.drawing_tools'],
     package_dir = {'parametricSynthesis': 'parametricSynthesis'}
 )
