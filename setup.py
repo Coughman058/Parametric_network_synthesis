8e98451cf0d011ae756945f71454dd309d18d736
Ryan Kaufman <51339132+Coughman058@users.noreply.github.com>
2023-12-06 09:56:57 -0500

    adding requirements in setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name='Parametric_Network_Synthesis',
     version='0.0.1',
     packages = find_namespace_packages(where = 'src'),
-    package_dir={"": "src"}
+    package_dir={"": "src"},
+    install_requires=['schemdraw', 'numpy', 'matplotlib']
 )
