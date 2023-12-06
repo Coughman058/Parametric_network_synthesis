91ca7217d6cfc90cbcac94f459188ae78a25a7aa
Ryan Kaufman <51339132+Coughman058@users.noreply.github.com>
2023-12-06 11:52:46 -0500

    Testing out tensorwaves fast_lambdify to speed up scattering plotting

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='0.0.1',
     packages = find_namespace_packages(where = 'src'),
     package_dir={"": "src"},
-    install_requires=['schemdraw', 'numpy', 'matplotlib']
+    install_requires=['schemdraw', 'numpy', 'matplotlib', 'tensorwaves']
 )
