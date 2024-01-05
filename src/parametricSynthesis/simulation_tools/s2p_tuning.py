191e643046c1287755fe6059dc2facafb89c8ac5
Ryan Kaufman <51339132+Coughman058@users.noreply.github.com>
2024-01-05 18:56:17 -0500

    !Fix: app not quite working, inductance not scanning?

--- src/parametricSynthesis/simulation_tools/s2p_tuning.py
+++ src/parametricSynthesis/simulation_tools/s2p_tuning.py
@@ -2,14 +2,10 @@
 The goal of this module is to implement live plotly analysis of HFSS-exported s2p files in a google colaboratory notebook.
 '''
 
-import plotly.express as px
 import pandas as pd
-import skrf as rf
 import sympy as sp
 import numpy as np
-import plotly.express as px
 import plotly.graph_objects as go
-from ipywidgets import interact
 from ipywidgets import widgets
 from ..simulation_tools.HFSS_analysis import interpolated_network_with_inverter_from_filename as net_with_inverter
 # L_vals = np.arange(0.5, 1.1, 0.05)*1e-9
@@ -21,37 +17,37 @@ def sweep_core_inductance_and_inversion_rate_from_filelist(filenames_dict_with_v
                                                            J_vals:np.ndarray,
                                                            omega0_val:float,
                                                            dw_val:float):
-  '''
-  takes in a dictionary of filenames and sweep values for a 1D sweep of s2p files,
-  and returns a list of functions that return the S matrix of the network
-  as a function of the core inductance and inversion rate of the inverter
-  :param filenames_CC:
-  :return:
-  '''
-  filenames_sweep = filenames_dict_with_vals.keys()
-  sweep_vals = filenames_dict_with_vals.values()
-  L_array_sym, J_array_sim = sp.symbols("L_pa, J_pa")
-  HFSS_sweep_sims = [net_with_inverter(filename=filename,
+    '''
+    takes in a dictionary of filenames and sweep values for a 1D sweep of s2p files,
+    and returns a list of functions that return the S matrix of the network
+    as a function of the core inductance and inversion rate of the inverter
+    :param filenames_CC:
+    :return:
+    '''
+    filenames_sweep = filenames_dict_with_vals.keys()
+    sweep_vals = filenames_dict_with_vals.values()
+    L_array_sym, J_array_sim = sp.symbols("L_pa, J_pa")
+    HFSS_sweep_sims = [net_with_inverter(filename=filename,
                                        L_sym=L_array_sym,
                                        inv_J_sym=J_array_sim,
                                        dw=dw_val,
                                        omega0_val=omega0_val) for filename in filenames_sweep]
-  #bundle the data into a pandas dataframe for plotly
-  print("DEBUG: L_vals in plotly_1D_sweep is: ", L_vals)
-  data_list = []
-  for L in L_vals:
-    for J in J_vals:
-      for sweep_val, HFSS in zip(sweep_vals, HFSS_sweep_sims):
-        omega_arr = np.linspace(-1e9,1e9,401)*2*np.pi + omega0_val
-        L_arr = L*np.ones(omega_arr.size)
-        Jpa_arr = J*np.ones(omega_arr.size)
-        Smtx_res = HFSS.evaluate_Smtx(L_arr, Jpa_arr, omega_arr)
-        data = pd.DataFrame({'Signal Frequency (GHz)': omega_arr/2/np.pi/1e9,
-                              'Array Inductance (nH)': L_arr*1e9,
-                              'Inversion Rate': Jpa_arr,
-                              sweep_parameter_name: sweep_val*np.ones(omega_arr.size),
-                              'S11magDB': 20*np.log10(np.abs(Smtx_res[0,0]))})
-        data_list.append(data)
+    #bundle the data into a pandas dataframe for plotly
+    print("DEBUG: L_vals in plotly_1D_sweep is: ", L_vals)
+    data_list = []
+    for L in L_vals:
+      for J in J_vals:
+        for sweep_val, HFSS in zip(sweep_vals, HFSS_sweep_sims):
+          omega_arr = np.linspace(-1e9,1e9,401)*2*np.pi + omega0_val
+          L_arr = L*np.ones(omega_arr.size)
+          Jpa_arr = J*np.ones(omega_arr.size)
+          Smtx_res = HFSS.evaluate_Smtx(L_arr, Jpa_arr, omega_arr)
+          data = pd.DataFrame({'Signal Frequency (GHz)': omega_arr/2/np.pi/1e9,
+                                'Array Inductance (nH)': L_arr*1e9,
+                                'Inversion Rate': Jpa_arr,
+                                sweep_parameter_name: sweep_val*np.ones(omega_arr.size),
+                                'S11magDB': 20*np.log10(np.abs(Smtx_res[0,0]))})
+          data_list.append(data)
 
     total_data = pd.concat(data_list, ignore_index = True, axis = 0)
 
