7b977a70d1b4b54a10990e8bed67c938202e7246
Ryan Kaufman <51339132+rkaufman058@users.noreply.github.com>
2024-01-26 10:50:49 -0500

    !Fix: when looking at the impedance seen by the amplifier mode, we need to include the negative capacitance from the first capacitive inverter if it is a cap_cpld circuit

--- src/parametricSynthesis/network_tools/network_synthesis.py
+++ src/parametricSynthesis/network_tools/network_synthesis.py
@@ -726,7 +726,7 @@ class Network:
         ABCD = self.total_passive_ABCD(array=False, add_index = add_index)
         if self.Ftype == 'cap_cpld_lumped':
             negative_first_cap_symbol = sp.symbols('C_comp')
-            negative_first_cap = Capacitor(omega_symbol=self.inv_el.omega_sym, symbole = negative_first_cap_symbol, val = -self.CC_arr[0])
+            negative_first_cap = Capacitor(omega_symbol=self.omega_from_inverter, symbole = negative_first_cap_symbol, val = -self.CC_arr[0])
             ABCD_comp = negative_first_cap.ABCDShunt()
             self.net_subs.append(negative_first_cap_symbol, negative_first_cap.val)
             ABCD_total = ABCD * ABCD_comp
