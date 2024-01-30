59a36c89058cbf0eb8faf9a39a66385286887c95
Ryan Kaufman <51339132+rkaufman058@users.noreply.github.com>
2024-01-29 21:06:54 -0500

    !Feature: interpolating multi-parameter multi-goal HFSS sweeps for design libraries. (aka fine, I'll make my own PDK)

--- src/parametricSynthesis/simulation_tools/parameter_interpolation.py
+++ src/parametricSynthesis/simulation_tools/parameter_interpolation.py
@@ -14,14 +14,16 @@ def import_HFSS_csv(filename, display = False):
     '''
     imports an HFSS csv file and returns a pandas dataframe
     '''
+    file = pd.read_csv(filename)
     if display:
         print("importing", filename)
-        file = pd.read_csv(filename)
         print(file)
-
     return file
 
-def interpolate_nd_hfss_mgoal_res(df, verbose = True, dep_var_num = 1):
+def interpolate_nd_hfss_mgoal_res(df,
+                                  verbose = True,
+                                  exclude_columns = None,
+                                  dep_var_num = 1):
     '''
     Expects a pandas dataframe with columns var1name, var2name, etc and goalname, outputs a function which will interpolate
     the goalname as a function of all varnames
@@ -33,26 +35,39 @@ def interpolate_nd_hfss_mgoal_res(df, verbose = True, dep_var_num = 1):
     if verbose:
         print(f"Interpolating {goalnames} as function of {varnames[0:-dep_var_num]}")
     interpfuncs = []
+
     for i, goalname in enumerate(goalnames):
         ivarcpls = df.to_numpy()[:,0:ivar_num]
-        dvarlist = df.to_numpy()[:,ivar_num+i]
+        if exclude_columns is not None:
+            print("old cols:", ivarcpls)
+            ivarcpls = np.delete(ivarcpls, exclude_columns, axis = 1)
+            print("new cols:", ivarcpls)
+        dvarlist = df.to_numpy()[:,-dep_var_num+i]
         interpfunc = LinearNDInterpolator(ivarcpls, dvarlist)
         interpfuncs.append(interpfunc)
     return interpfuncs
 
 
 
-def display_interpolation_result(interpfuncs, df, optimization = []):
+def display_interpolation_result(interpfuncs, df, exclude_column = None, optimization = [], primary_cols = [0,1], plot_constrained = False):
     '''
     Plots the interpolation function result and the points that it was swept over
     '''
 
-    fig, axs = plt.subplots(nrows = 1, ncols = len(interpfuncs))
     dep_var_num = len(interpfuncs)
-    if dep_var_num == 1:
-        axs = [axs]
+    if dep_var_num <=2:
+        fig, axs = plt.subplots(nrows=1, ncols=len(interpfuncs))
+        if dep_var_num == 1:
+            axs = [axs]
     # print(f"dep_var=_num: {dep_var_num}")
+    '''
+    always ignore a column first, then talk about the constraints on the rest
+    !TODO: find a way to unify these two cases
+    '''
     varnames = np.array([df.columns[i] for i in range(len(df.columns))])
+    if exclude_column is not None:
+        varnames = np.delete(varnames, exclude_column)
+
     ivarnames = varnames[0:-dep_var_num]
     goalnames = varnames[-dep_var_num:]
     for j in range(dep_var_num):
@@ -68,6 +83,11 @@ def display_interpolation_result(interpfuncs, df, optimization = []):
             ax.plot(x,y)
             ax.set_xlabel(varnames[0])
             ax.set_ylabel(goalname)
+            if len(optimization) > 0:
+                ax.scatter(opt_res[0], opt_res[1], color='r')
+                ax.set_title(goalname + f'\nopt is {interpfunc(opt_res)}\nat {opt_res}')
+            else:
+                ax.set_title(goalname)
 
         elif interpfunc.points[0].size == 2:
             x = np.linspace(np.min(interpfunc.points[:,0]), np.max(interpfunc.points[:,0]), 101)
@@ -79,26 +99,79 @@ def display_interpolation_result(interpfuncs, df, optimization = []):
             ax.set_xlabel(varnames[0])
             ax.set_ylabel(varnames[1])
             plt.colorbar(im)
+            if len(optimization) > 0:
+                ax.scatter(opt_res[0], opt_res[1], color='r')
+                ax.set_title(goalname + f'\nopt is {interpfunc(opt_res)}\nwith {opt_res}')
+            else:
+                ax.set_title(goalname)
         else:
-            raise Exception("Can't display interpolation result for more than 2 dimensions")
+            print ("input more than three dimensional")
+            print(f"Optimization results for {goalname} = {interpfunc(opt_res)}: ")
+            print(f"{ivarnames} = {opt_res}")
+            if plot_constrained:
+                print(f"Plotting constrained sweep for simultaneous optimization")
+                constrained_vars = [varnames[i] for i in range(len(varnames)) if i not in primary_cols]
+                constrained_vals = [opt_res[i] for i in range(len(opt_res)) if i not in primary_cols]
+                print(f"Constrained variables: {constrained_vars} = {constrained_vals}")
+                def constrained_interpfunc(x1, x2, primary_cols = primary_cols):
+                    input_arr = []
+                    for input_col in range(np.shape(opt_res)[-1]):
+                        if input_col == primary_cols[0]:
+                            input_arr.append(x1)
+                        elif input_col == primary_cols[1]:
+                            input_arr.append(x2)
+                        else:
+                            input_arr.append(np.ones(x1.shape)*opt_res[input_col])
+                    input_arr = np.array(input_arr)
+                    print(np.shape(input_arr))
+                    return interpfunc(*input_arr)
 
-        if len(optimization) > 0:
-            ax.scatter(opt_res[0], opt_res[1], color='r')
-            ax.set_title(goalname+f'\nopt for {interpfunc(opt_res)}\nat {opt_res}')
-        else:
-            ax.set_title(goalname)
+                x = np.linspace(np.min(interpfunc.points[:, primary_cols[0]]), np.max(interpfunc.points[:, primary_cols[0]]), 101)
+                y = np.linspace(np.min(interpfunc.points[:, primary_cols[1]]), np.max(interpfunc.points[:, primary_cols[1]]), 101)
+                X, Y = np.meshgrid(x, y)
+                Z = constrained_interpfunc(X, Y)
+                im = ax.contourf(X, Y, Z)
+                print("\n\nplotted image\n\n")
+                print(Z)
+                ax.scatter(interpfunc.points[:, primary_cols[0]], interpfunc.points[:, primary_cols[1]], color='k')
+                ax.set_xlabel(varnames[primary_cols[0]])
+                ax.set_ylabel(varnames[primary_cols[1]])
+                plt.colorbar(im)
+                if len(optimization) > 0:
+                    ax.scatter(opt_res[primary_cols[0]], opt_res[primary_cols[1]], color='r')
+                    ax.set_title(goalname + f'\nopt is {interpfunc(opt_res)}\nwith {opt_res}\nconstrained by {constrained_vars} =\n{constrained_vals}')
+                else:
+                    ax.set_title(goalname)
 
     return fig, axs
 
-def optimize_for_goal(interpfunc, goal_val, p0 = None):
+def optimize_for_goal(interpfuncs, goal_vals, p0 = None, optimize_all = False, all_weights = None, primary_cols = [0,1]):
     '''
     Finds the optimal value of the independent variables to get the goal_val
     '''
-    def goal_diff(x):
-        return np.abs(interpfunc(*x) - goal_val)
+    res_arr = []
+    if optimize_all == False:
+        for interpfunc, goal_val in zip(interpfuncs, goal_vals):
+            def goal_diff(x):
+                return np.abs(interpfunc(*x) - goal_val)
+            if p0 is None:
+                p0 = interpfunc.points[interpfunc.points.size//2]
+            res = minimize(goal_diff, p0, method='nelder-mead')
+            res_arr.append(res.x)
+    else:
+        if all_weights is None:
+            all_weights = np.ones(len(interpfuncs))
+        def goal_diff(x):
+            # print("all_weights[0]: ", all_weights[0])
+            to_sum = []
+            for i, interpfunc in enumerate(interpfuncs):
+                cost = np.sum(np.abs(interpfunc(*x) - goal_vals[i])/goal_vals[i])
+                to_sum.append(cost)
+            return np.sum(to_sum)
 
-    #most of these functions are monotonic, so we can just use Nelder-Mead from the middle of the interpolation function
-    if p0 is None:
-        p0 = interpfunc.points[np.size(interpfunc.points,0)//2]
-    res = minimize(goal_diff, p0, method = 'Nelder-Mead')
-    return res.x
+        if p0 is None:
+            p0 = interpfuncs[0].points[len(interpfuncs[0].points) // 2]
+        res = minimize(goal_diff, p0, method='nelder-mead', options = {'tol': 1e-6})
+        # print(res
+        res_arr.append(res.x)
+    return res_arr
--- testing/HFSS_parameter_import_testing.py
+++ testing/HFSS_parameter_import_testing.py
@@ -6,16 +6,58 @@ from parametricSynthesis.simulation_tools import parameter_interpolation as pi
 import matplotlib.pyplot as plt
 plt.ioff()
 
-testfile_2d = pi.import_HFSS_csv(r"C:\Users\Hatlab-RRK\Documents\GitHub\Parametric_network_synthesis\testing\Parameter_interp_files\tlineZo_cpw_width_gap_freq_Sa_NoWirebond.csv", display = True)
-# testfile_1d = pi.import_HFSS_csv(r"C:\Users\Hatlab-RRK\Documents\GitHub\Parametric_network_synthesis\testing\Parameter_interp_files\tlineZo_Sa_NoWirebond.csv", display = True)
-testfile = testfile_2d
+testfile_3d = pi.import_HFSS_csv(r"C:\Users\Hatlab-RRK\Documents\GitHub\Parametric_network_synthesis\testing\Parameter_interp_files\20240129_tlin_sweep_sa_no_wb_oneloop.csv")
+testfile_3d_with_fixed_gap = pi.import_HFSS_csv(r"C:\Users\Hatlab-RRK\Documents\GitHub\Parametric_network_synthesis\testing\Parameter_interp_files\20240129_tlin_sweep_sa_oneloop_fixed_gap.csv")
+testfile_2d = pi.import_HFSS_csv(r"C:\Users\Hatlab-RRK\Documents\GitHub\Parametric_network_synthesis\testing\Parameter_interp_files\tlineZo_cpw_width_gap_freq_Sa_NoWirebond.csv")
+testfile_1d = pi.import_HFSS_csv(r"C:\Users\Hatlab-RRK\Documents\GitHub\Parametric_network_synthesis\testing\Parameter_interp_files\tlineZo_Sa_NoWirebond.csv")
 dep_var_num = 2
-interpfuncs = pi.interpolate_nd_hfss_mgoal_res(testfile, dep_var_num=2)
-opt_res_arr = []
-goals = [60, 7e9]
-for i, interpfunc in enumerate(interpfuncs):
-    opt_res_x = pi.optimize_for_goal(interpfunc, goals[i])
-    opt_res_arr.append(opt_res_x)
-fig, axs = pi.display_interpolation_result(interpfuncs, testfile, optimization=opt_res_arr)
+
+
+
+#independent optimization
+# cols_to_exclude = [1]
+# interpfuncs = pi.interpolate_nd_hfss_mgoal_res(testfile_1d, exclude_columns = col_to_exclude, dep_var_num=dep_var_num)
+# opt_res_arr = []
+# goals = [60, 7e9]
+# for i, interpfunc in enumerate(interpfuncs):
+#     opt_res_x = pi.optimize_for_goal([interpfunc], goals[i], optimize_all = False)[0]
+#     opt_res_arr.append(opt_res_x)
+# fig, axs = pi.display_interpolation_result(interpfuncs, testfile_1d, optimization=opt_res_arr, exclude_column = cols_to_exclude)
+# plt.show()
+
+#simultaneous optimization, 2d
+# goals = [80, 7e9]
+# cols_to_exclude = [0,1]
+# primary_cols = [0,2]
+# #find a pair here, then take the length and hold it constant
+# interpfuncs = pi.interpolate_nd_hfss_mgoal_res(testfile_3D_with_fixed_gap, exclude_columns = cols_to_exclude, dep_var_num=dep_var_num)
+# print("Interpolation successful")
+# opt_res_vals = pi.optimize_for_goal(interpfuncs, goals, optimize_all=True)[0]
+# # opt_res_vals = [res.x for res in opt_res]
+# display_funcs = interpfuncs
+# print("Optimization successful")
+# print("opt_res_arr",opt_res_vals)
+# fig, axs = pi.display_interpolation_result(display_funcs, testfile_3D_with_fixed_gap,
+#                                            optimization=[opt_res_vals, opt_res_vals],
+#                                            exclude_column = cols_to_exclude)
+# plt.show()
+
+#constrained optimization, 3d
+cols_to_exclude = [1]
+primary_cols = [0,2]
+goals = [40, 7e9]
+#find a pair here, then take the length and hold it constant
+interpfuncs = pi.interpolate_nd_hfss_mgoal_res(testfile_3d, exclude_columns = cols_to_exclude, dep_var_num=dep_var_num)
+print("Interpolation successful")
+opt_res_vals = pi.optimize_for_goal(interpfuncs, goals, optimize_all=True)[0]
+display_funcs = interpfuncs
+print("Optimization successful")
+print("opt_res_arr",opt_res_vals)
+fig, axs = pi.display_interpolation_result(display_funcs, testfile_3d,
+                                           optimization=[opt_res_vals, opt_res_vals],
+                                           exclude_column = cols_to_exclude,
+                                           primary_cols = primary_cols,
+                                           plot_constrained=True)
 plt.show()
+
 breakpoint()
